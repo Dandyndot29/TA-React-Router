e744633f905a6585f313376170640ae50ee9317f
Dandyndot29 <dandysetiawanptr@gmail.com>
2022-03-17 12:45:57 +0800

    ta react router

---
+++ soal-01/src/routes.js
@@ -0,0 +1,22 @@
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import MovieList from './pages/MovieList';
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    element: Home,
+  },
+
+  {
+    name: 'movie list',
+    path: '/movies',
+    element: MovieList,
+  },
+  {
+    
+  },
+];
+
+export default routes;
